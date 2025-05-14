4fae83682ce8669fc5677a4ee3275ae951c9b5cd
bgogeta007 <bgogeta007@gmail.com>
2025-05-14 17:26:05 +0100

    Update ProgressPhotos component with URL refresh mechanism

--- src/components/admin/ChallengesTab.tsx
+++ src/components/admin/ChallengesTab.tsx
@@ -184,7 +184,7 @@ const ChallengesTab: React.FC = () => {
       </div>
 
       {/* Filters */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         <div className="relative">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
           <input
