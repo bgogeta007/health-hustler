e59f98e56273f9c93c393633c1c35661a077d0e8
bgogeta007 <bgogeta007@gmail.com>
2025-05-14 15:59:28 +0100

    Update ProgressPhotos component with URL refresh mechanism

--- src/pages/ProgressPhotos.tsx
+++ src/pages/ProgressPhotos.tsx
@@ -30,7 +30,6 @@ const ProgressPhotos: React.FC = () => {
 
   useEffect(() => {
     fetchPhotos();
-    refreshSignedUrls();
 
     // Set up interval to refresh URLs every 45 minutes (before the 1-hour expiry)
     urlRefreshInterval.current = setInterval(refreshSignedUrls, 45 * 60 * 1000);
