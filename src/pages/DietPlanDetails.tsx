b62564548e5275c61febafbf9f5337b423ea8b47
bgogeta007 <bgogeta007@gmail.com>
2025-05-14 15:15:31 +0100

    Updated DietPlanDetails.tsx

--- src/pages/DietPlanDetails.tsx
+++ src/pages/DietPlanDetails.tsx
@@ -1335,7 +1335,7 @@ const DietPlanDetails: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen pt-24 bg-gray-50 dark:bg-gray-900 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen pt-24 bg-gray-50 dark:bg-gray-900 pb-16 px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
         <div className="bg-white dark:bg-gray-800 rounded-lg shadow-xl overflow-hidden">
           {/* Hero Section */}
