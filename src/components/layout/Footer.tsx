722597759853ec54a547f48ea02d03156fcc4527
bgogeta007 <bgogeta007@gmail.com>
2025-05-14 16:37:18 +0100

    Update ProgressPhotos component with URL refresh mechanism

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -138,7 +138,7 @@ const Footer: React.FC = () => {
           </div>
         </div>
 
-        <div className="border-t border-gray-700 mt-3 pt-8 text-center text-gray-500 dark:text-gray-400">
+        <div className="border-t border-gray-700  text-center text-gray-500 dark:text-gray-400">
           <p>
             &copy; {new Date().getFullYear()} GreenLean. All rights reserved.
           </p>
